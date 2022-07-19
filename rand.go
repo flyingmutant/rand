a237867a65ab0df5dcd4bce48af26f38a5af1d61
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2022-07-19 03:07:32 +0300

    Make Uint64n() match original algorithm

--- rand.go
+++ rand.go
@@ -216,10 +216,10 @@ func (r *Rand) Uint64n(n uint64) uint64 {
 	// "An optimal algorithm for bounded random integers" by Stephen Canon, https://github.com/apple/swift/pull/39143
 	// making second multiplication unconditional makes the function inlineable, but slows things down for small n
 	res, frac := bits.Mul64(n, r.next())
-	if frac < n {
-		hi, _ := bits.Mul64(n, r.next())
-		_, carry := bits.Add64(frac, hi, 0)
-		res += carry
+	if frac <= -n {
+		return res
 	}
-	return res
+	hi, _ := bits.Mul64(n, r.next())
+	_, carry := bits.Add64(frac, hi, 0)
+	return res + carry
 }
