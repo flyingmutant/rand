cf7ba23b4e9dd623c8b617bc73aac096533ae5af
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2022-08-06 15:39:00 +0300

    More benchmarking fun

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module pgregory.net/rand
 go 1.18
 
 require (
+	github.com/valyala/fastrand v1.1.0
 	golang.org/x/exp v0.0.0-20220713135740-79cabaa25d75
 	pgregory.net/rapid v0.4.8
+
 )
---
+++ rand_fast_test.go
@@ -0,0 +1,24 @@
+// Copyright 2022 Gregory Petrosyan <gregory.petrosyan@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//go:build benchfast
+
+package rand_test
+
+import (
+	"github.com/valyala/fastrand"
+	"testing"
+)
+
+func BenchmarkIntn(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		var s uint32
+		for pb.Next() {
+			s = fastrand.Uint32n(small)
+		}
+		sinkUint32 = s
+	})
+}
