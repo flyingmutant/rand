38fd8b8e433d31be50bd408a300dda2202365c28
Gregory Petrosyan <pgregory@pgregory.net>
2024-08-13 14:26:39 +0300

    Special-case rand64() for Go 1.22

--- global_go122.go
+++ global_go122.go
@@ -1,15 +1,15 @@
-// Copyright 2022 Gregory Petrosyan <gregory.petrosyan@gmail.com>
+// Copyright 2024 Gregory Petrosyan <gregory.petrosyan@gmail.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//go:build go1.19 && !go1.22 && !unsafe
+//go:build go1.22 && !unsafe
 
 package rand
 
-import "hash/maphash"
+import "math/rand/v2"
 
 func rand64() uint64 {
-	return maphash.Bytes(maphash.MakeSeed(), nil)
+	return rand.Uint64()
 }
--- global_unsafe_go122.go
+++ global_unsafe_go122.go
@@ -1,10 +1,10 @@
-// Copyright 2022 Gregory Petrosyan <gregory.petrosyan@gmail.com>
+// Copyright 2024 Gregory Petrosyan <gregory.petrosyan@gmail.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//go:build unsafe && !go1.22
+//go:build unsafe && go1.22
 
 package rand
 
@@ -14,5 +14,5 @@ import (
 
 // if you *really* want to win the benchmarks game:
 
-//go:linkname rand64 runtime.fastrand64
+//go:linkname rand64 runtime.rand
 func rand64() uint64
